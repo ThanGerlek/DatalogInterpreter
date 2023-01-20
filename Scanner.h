4e89115aacd020904162d287ae0e5c83eb2ff920
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-01-19 22:07:46 -0700

    Whitespace removal & a test of token checking

--- Scanner.h
+++ Scanner.h
@@ -1,6 +1,7 @@
 #ifndef PROJECT1_T2K_SCANNER_H
 #define PROJECT1_T2K_SCANNER_H
 
+#include <cctype>
 #include <vector>
 #include "Token.h"
 
@@ -9,17 +10,36 @@ class Scanner
 private:
     std::string input;
     std::vector<Token> tokens;
+    int currentLine;
 
 public:
-    Scanner(const std::string &input) : input(input){};
+    Scanner(const std::string &input) : input(input), currentLine(1){};
 
     Token scanToken()
     {
-        TokenType type = COMMA;
-        std::string value = ",";
-        int line = 1;
+        TokenType type = UNDEFINED;
+        std::string value = "";
+        int line = -1;
+
+        // Remove leading whitespace
+        while (std::isspace(this->input.at(0)))
+        {
+            this->input = this->input.substr(1);
+        }
 
         // TODO
+
+        // Check for COMMA token
+        if (input.at(0) == ',')
+        {
+            type = COMMA;
+            value = ",";
+            line = currentLine;
+
+            // Prep for next token
+            currentLine++;
+            input = input.substr(1);
+        }
 
         return Token(type, value, line);
     }
