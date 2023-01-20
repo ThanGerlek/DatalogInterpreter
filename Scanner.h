53205e815a1b8d56830d724f7890bbb3d1d02fba
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-01-19 21:48:28 -0700

    Outlined Scanner class

--- Scanner.h
+++ Scanner.h
@@ -1,17 +1,28 @@
 #ifndef PROJECT1_T2K_SCANNER_H
 #define PROJECT1_T2K_SCANNER_H
 
+#include <vector>
 #include "Token.h"
 
 class Scanner
 {
-public:
-    Scanner();
-    ~Scanner();
-    // TODO
-
 private:
-    //
+    std::string input;
+    std::vector<Token> tokens;
+
+public:
+    Scanner(const std::string &input) : input(input){};
+
+    Token scanToken()
+    {
+        TokenType type = COMMA;
+        std::string value = ",";
+        int line = 1;
+
+        // TODO
+
+        return Token(type, value, line);
+    }
 };
 
 #endif
