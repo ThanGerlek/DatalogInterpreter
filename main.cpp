5f9c2da99360a21473120b7f60a042f50e6d3eb8
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-01-20 14:54:12 -0700

    Test main() function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #ifndef PROJECT1_T2K_MAIN_CPP
 #define PROJECT1_T2K_MAIN_CPP
 
+#include <fstream>
+#include <iostream>
 #include "Scanner.h"
 
 int main(int argc, char *argv[])
 {
-    // TODO
+    if (argc < 2)
+    {
+        std::cout << "Please include a file name." << std::endl;
+        throw;
+    }
+
+    std::string filename = argv[1];
+    filename = "input.txt"; // TODO Un-hardcode input file
+    std::ifstream ifs = std::ifstream(filename);
+    if (!ifs.is_open())
+    {
+        std::cout << "Failed to open file." << std::endl;
+        throw;
+    }
+
+    //? Can this be more efficient?
+    std::stringstream input;
+    std::string line;
+    while (std::getline(ifs, line))
+    {
+        input << std::endl
+              << line;
+    }
+
+    Scanner scanner = Scanner(input.str());
+
+    Token token;
+    while (scanner.hasNext())
+    {
+        token = scanner.scanToken();
+        std::cout << token.toString() << std::endl;
+    }
+
     return 0;
 }
 
