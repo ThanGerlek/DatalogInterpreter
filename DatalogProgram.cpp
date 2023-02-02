c16e469c6c1c636b2a138709ecc7dbf62521c534
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-02-02 16:37:38 -0700

    Fixed typos

--- DatalogProgram.cpp
+++ DatalogProgram.cpp
@@ -4,15 +4,15 @@
 #include <sstream>
 #include "DatalogProgram.h"
 
-void DatalogProgram::addScheme(Predicate scheme) { schemes.push_back(scheme); };
-void DatalogProgram::addFact(Predicate fact) { facts.push_back(fact); };
-void DatalogProgram::addQuery(Predicate query) { queries.push_back(query); };
-void DatalogProgram::addRule(Rule rule) { rules.push_back(rule); };
+void DatalogProgram::addScheme(Predicate scheme) { schemes.push_back(scheme); }
+void DatalogProgram::addFact(Predicate fact) { facts.push_back(fact); }
+void DatalogProgram::addQuery(Predicate query) { queries.push_back(query); }
+void DatalogProgram::addRule(Rule rule) { rules.push_back(rule); }
 
-std::vector<Predicate> *DatalogProgram::getSchemes() { return &schemes; };
-std::vector<Predicate> *DatalogProgram::getFacts() { return &facts; };
-std::vector<Predicate> *DatalogProgram::getQueries() { return &queries; };
-std::vector<Rule> *DatalogProgram::getRules() { return &rules; };
+std::vector<Predicate> *DatalogProgram::getSchemes() { return &schemes; }
+std::vector<Predicate> *DatalogProgram::getFacts() { return &facts; }
+std::vector<Predicate> *DatalogProgram::getQueries() { return &queries; }
+std::vector<Rule> *DatalogProgram::getRules() { return &rules; }
 
 std::string DatalogProgram::toString()
 {
