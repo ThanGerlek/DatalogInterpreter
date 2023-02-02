46986a9e2010e60e72f087988a413eef331f2e39
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-02-02 12:51:30 -0700

    Added Parser's tokens and program fields

--- Parser.h
+++ Parser.h
@@ -8,13 +8,16 @@
 class Parser
 {
 public:
-    Parser(const std::vector<Token> &tokens, DatalogProgram &program){};
+    Parser(const std::vector<Token> *_tokens, DatalogProgram *_program) : tokens(_tokens), program(_program){};
     ~Parser(){};
     void parse();
 
 private:
+    const std::vector<Token> *tokens;
+    DatalogProgram *program;
     int location; // Index of the next token
     std::stack<int> savedLocs;
+
     void saveLoc();
     void restoreLoc();
 
