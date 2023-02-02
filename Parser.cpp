dd73291a77fed710fb1863995f3adfa8e7486f4c
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-02-02 12:48:16 -0700

    Outlined recursive-descent grammar functions

--- Parser.cpp
+++ Parser.cpp
@@ -1,6 +1,158 @@
 #ifndef T2K_236PROJECT_PARSER_CPP
 #define T2K_236PROJECT_PARSER_CPP
 
-//
+#include "Parser.h"
+
+void Parser::saveLoc()
+{
+    // TODO
+}
+
+void Parser::restoreLoc()
+{
+    // TODO
+}
+
+/**
+ * Parse this Parser's DatalogProgram using recursive descent.
+ * Throws an error if the program is invalid.
+ */
+void Parser::parse()
+{
+    // TODO
+}
+
+// Recursive descent grammar functions
+
+/* Datalog Grammar:
+
+datalogProgram	->	SCHEMES COLON scheme schemeList
+                FACTS COLON factList
+                RULES COLON ruleList
+                QUERIES COLON query queryList
+            EOF
+
+schemeList	->	scheme schemeList | lambda
+factList	->	fact factList | lambda
+ruleList	->	rule ruleList | lambda
+queryList	->	query queryList | lambda
+
+scheme   	-> 	ID LEFT_PAREN ID idList RIGHT_PAREN
+fact    	->	ID LEFT_PAREN STRING stringList RIGHT_PAREN PERIOD
+rule    	->	headPredicate COLON_DASH predicate predicateList PERIOD
+query	        ->      predicate Q_MARK
+
+headPredicate	->	ID LEFT_PAREN ID idList RIGHT_PAREN
+predicate	->	ID LEFT_PAREN parameter parameterList RIGHT_PAREN
+
+predicateList	->	COMMA predicate predicateList | lambda
+parameterList	-> 	COMMA parameter parameterList | lambda
+stringList	-> 	COMMA STRING stringList | lambda
+idList  	-> 	COMMA ID idList | lambda
+
+parameter	->	STRING | ID
+*/
+
+bool Parser::datalogProgram() const
+{
+    /*
+    datalogProgram	->	SCHEMES COLON scheme schemeList
+                        FACTS COLON factList
+                        RULES COLON ruleList
+                        QUERIES COLON query queryList
+                        EOF
+    */
+    // TODO
+}
+
+bool Parser::schemeList() const
+{
+    // schemeList -> scheme schemeList | lambda
+    // TODO
+}
+
+bool Parser::factList() const
+{
+    // factList -> fact factList | lambda
+    // TODO
+}
+
+bool Parser::ruleList() const
+{
+    // ruleList -> rule ruleList | lambda
+    // TODO
+}
+
+bool Parser::queryList() const
+{
+    // queryList -> query queryList | lambda
+    // TODO
+}
+
+bool Parser::scheme() const
+{
+    // scheme -> ID LEFT_PAREN ID idList RIGHT_PAREN
+    // TODO
+}
+
+bool Parser::fact() const
+{
+    // fact -> ID LEFT_PAREN STRING stringList RIGHT_PAREN PERIOD
+    // TODO
+}
+
+bool Parser::rule() const
+{
+    // rule -> headPredicate COLON_DASH predicate predicateList PERIOD
+    // TODO
+}
+
+bool Parser::query() const
+{
+    // query -> predicate Q_MARK
+    // TODO
+}
+
+bool Parser::headPredicate() const
+{
+    // headPredicate -> ID LEFT_PAREN ID idList RIGHT_PAREN
+    // TODO
+}
+
+bool Parser::predicate() const
+{
+    // predicate -> ID LEFT_PAREN parameter parameterList RIGHT_PAREN
+    // TODO
+}
+
+bool Parser::predicateList() const
+{
+    // predicateList -> COMMA predicate predicateList | lambda
+    // TODO
+}
+
+bool Parser::parameterList() const
+{
+    // parameterList -> COMMA parameter parameterList | lambda
+    // TODO
+}
+
+bool Parser::stringList() const
+{
+    // stringList -> COMMA STRING stringList | lambda
+    // TODO
+}
+
+bool Parser::idList() const
+{
+    // idList -> COMMA ID idList | lambda
+    // TODO
+}
+
+bool Parser::parameter() const
+{
+    // parameter -> STRING | ID
+    // TODO
+}
 
 #endif
--- Parser.h
+++ Parser.h
@@ -1,6 +1,7 @@
 #ifndef T2K_236PROJECT_PARSER_H
 #define T2K_236PROJECT_PARSER_H
 
+#include <stack>
 #include "Scanner.h"
 #include "DatalogProgram.h"
 
@@ -12,7 +13,28 @@ public:
     void parse();
 
 private:
+    int location; // Index of the next token
+    std::stack<int> savedLocs;
+    void saveLoc();
+    void restoreLoc();
+
     // Recursive descent grammar functions
+    bool datalogProgram() const;
+    bool schemeList() const;
+    bool factList() const;
+    bool ruleList() const;
+    bool queryList() const;
+    bool scheme() const;
+    bool fact() const;
+    bool rule() const;
+    bool query() const;
+    bool headPredicate() const;
+    bool predicate() const;
+    bool predicateList() const;
+    bool parameterList() const;
+    bool stringList() const;
+    bool idList() const;
+    bool parameter() const;
 };
 
 #endif
