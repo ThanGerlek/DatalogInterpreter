c396f6976b349e610693eb0348875ccf8401a441
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-02-04 23:11:40 -0700

    Removed old debug code

--- main.cpp
+++ main.cpp
@@ -11,21 +11,18 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         std::cout << "Please include a file name." << std::endl;
-        throw;
+        return 0;
     }
 
     // Scan file
 
     std::string filename = argv[1];
-#ifdef DEBUG
-    std::cout << "Scanning file '" << filename << "'" << std::endl;
-#endif
 
     std::ifstream ifs = std::ifstream(filename);
     if (!ifs.is_open())
     {
         std::cout << "Failed to open file." << std::endl;
-        throw;
+        return 0;
     }
 
     Scanner scanner = Scanner(ifs);
@@ -34,27 +31,12 @@ int main(int argc, char *argv[])
     std::vector<Token> tokens;
     scanner.scan(tokens);
 
-    // DEBUG
-    for (std::vector<Token>::iterator i = tokens.begin(); i != tokens.end(); i++)
-    {
-        std::cout << i->toString() << "  ";
-    }
-    std::cout << std::endl;
-
-#ifdef DEBUG
-    std::cout << "Finished scanning, total tokens = " << tokens.size() << ". Attempting to parse..." << std::endl;
-#endif
-
     // Parse tokens
 
     DatalogProgram dlp;
     Parser p(&tokens, &dlp);
 
     p.parse();
-
-#ifdef DEBUG
-    std::cout << "Finished parsing." << std::endl;
-#endif
 
     return 0;
 }
