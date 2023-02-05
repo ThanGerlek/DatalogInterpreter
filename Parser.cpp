8c2ac42cc7d035016656fe701adb3e8a7519f869
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-02-04 17:42:50 -0700

    Implemented recursive descent

--- Parser.cpp
+++ Parser.cpp
@@ -109,37 +109,110 @@ void Parser::datalogProgram()
                         QUERIES COLON query queryList
                         EOF
     */
-    // TODO
+    // TODO test
+
+    std::cout << "dlp ";
+
+    match(SCHEMES);
+    match(COLON);
+    scheme();
+    schemeList();
+    match(FACTS);
+    match(COLON);
+    factList();
+    match(RULES);
+    match(COLON);
+    ruleList();
+    match(QUERIES);
+    match(COLON);
+    query();
+    queryList();
+    match(END_OF_FILE);
 }
 
 void Parser::schemeList()
 {
     // schemeList -> scheme schemeList | lambda
     // TODO
+    std::cout << "schemelist ";
+
+    saveLoc();
+    try
+    {
+        scheme();
+        schemeList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::factList()
 {
     // factList -> fact factList | lambda
     // TODO
+    std::cout << "factlist ";
+
+    saveLoc();
+    try
+    {
+        fact();
+        factList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::ruleList()
 {
     // ruleList -> rule ruleList | lambda
     // TODO
+
+    std::cout << "rulelist ";
+
+    saveLoc();
+    try
+    {
+        rule();
+        ruleList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::queryList()
 {
     // queryList -> query queryList | lambda
     // TODO
+    std::cout << "querylist ";
+
+    saveLoc();
+    try
+    {
+        query();
+        queryList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::scheme()
 {
+    std::cout << "scheme ";
+
     // scheme -> ID LEFT_PAREN ID idList RIGHT_PAREN
-    // TODO test scheme()
+    // TODO
     match(ID);
     match(LEFT_PAREN);
     match(ID);
@@ -151,66 +224,165 @@ void Parser::fact()
 {
     // fact -> ID LEFT_PAREN STRING stringList RIGHT_PAREN PERIOD
     // TODO
+
+    std::cout << "fact ";
+
+    match(ID);
+    match(LEFT_PAREN);
+    match(STRING);
+    stringList();
+    match(RIGHT_PAREN);
+    match(PERIOD);
 }
 
 void Parser::rule()
 {
     // rule -> headPredicate COLON_DASH predicate predicateList PERIOD
     // TODO
+
+    std::cout << "rule ";
+
+    headPredicate();
+    match(COLON_DASH);
+    predicate();
+    predicateList();
+    match(PERIOD);
 }
 
 void Parser::query()
 {
     // query -> predicate Q_MARK
     // TODO
+
+    std::cout << "query ";
+
+    predicate();
+    match(Q_MARK);
 }
 
 void Parser::headPredicate()
 {
     // headPredicate -> ID LEFT_PAREN ID idList RIGHT_PAREN
     // TODO
+    std::cout << "headPredicate ";
+
+    match(ID);
+    match(LEFT_PAREN);
+    match(ID);
+    idList();
+    match(RIGHT_PAREN);
 }
 
 void Parser::predicate()
 {
     // predicate -> ID LEFT_PAREN parameter parameterList RIGHT_PAREN
     // TODO
+
+    std::cout << "predicate ";
+
+    match(ID);
+    match(LEFT_PAREN);
+    parameter();
+    parameterList();
+    match(RIGHT_PAREN);
 }
 
 void Parser::predicateList()
 {
     // predicateList -> COMMA predicate predicateList | lambda
     // TODO
+    std::cout << "predicatelist ";
+
+    saveLoc();
+    try
+    {
+        match(COMMA);
+        predicate();
+        predicateList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::parameterList()
 {
+    std::cout << "parameterlist ";
+
     // parameterList -> COMMA parameter parameterList | lambda
     // TODO
+
+    saveLoc();
+    try
+    {
+        match(COMMA);
+        parameter();
+        parameterList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::stringList()
 {
+    std::cout << "strlist ";
+
     // stringList -> COMMA STRING stringList | lambda
     // TODO
+
+    saveLoc();
+    try
+    {
+        match(COMMA);
+        match(STRING);
+        stringList();
+    }
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::idList()
 {
+    std::cout << "idlist ";
+
     // idList -> COMMA ID idList | lambda
-    if (tokenType() == COMMA)
+
+    saveLoc();
+    try
     {
         match(COMMA);
         match(ID);
-        return idList();
+        idList();
     }
-    // else lambda
+    catch (Token)
+    {
+        restoreLoc();
+        // lambda
+    }
 }
 
 void Parser::parameter()
 {
+    std::cout << "parameter ";
+
     // parameter -> STRING | ID
     // TODO
+    if (tokenType() == STRING)
+    {
+        match(STRING);
+    }
+    else
+    {
+        match(ID);
+    }
 }
 
 #endif
