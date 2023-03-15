761db944a71cb4a3ba6d9d87ba88e4df22f12581
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-15 11:48:17 -0700

    Add Tuple and Scheme

---
+++ Scheme.h
@@ -0,0 +1,15 @@
+#ifndef CS236_LAB4_T2K_SCHEME_H
+#define CS236_LAB4_T2K_SCHEME_H
+
+#include <vector>
+#include <string>
+
+class Scheme : public std::vector<std::string>
+{
+public:
+    Scheme(vector<std::string> names) : vector<std::string>(names) 
+
+private:
+};
+
+#endif
---
+++ Tuple.h
@@ -0,0 +1,40 @@
+#ifndef CS236_LAB4_T2K_TUPLE_H
+#define CS236_LAB4_T2K_TUPLE_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "Scheme.h"
+
+class Tuple : public std::vector<std::string>
+{
+public:
+    Tuple(vector<std::string> values) : vector<std::string>(values){}; // TODO? Is this calling the base class's construstor?
+
+    std::string toString(const Scheme &scheme) const
+    {
+        const Tuple &tuple = *this; // TODO? When is this needed?
+
+        if (scheme.size() != tuple.size())
+        {
+            std::cerr << "Called toString() with a scheme and tuple of different sizes" << std::endl;
+            throw;
+        }
+
+        std::stringstream out;
+        for (int i = 0; i < tuple.size(); i++)
+        {
+            if (i != 0)
+            {
+                out << ", ";
+            }
+            out << scheme.at(i) << "=" << tuple.at(i);
+        }
+        return out.str();
+    }
+
+private:
+};
+
+#endif
