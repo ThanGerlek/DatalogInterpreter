c0d55246932c85830b94075324b77abf5fe807f8
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-28 14:18:36 -0700

    Made helper functions private

--- Relation.h
+++ Relation.h
@@ -12,6 +12,12 @@ private:
     std::string name;
     Scheme scheme;
     std::set<Tuple> tuples;
+    std::string joinNames(std::string left, std::string right);
+    const Scheme joinSchemes(const Scheme &left, const Scheme &right);
+    const Tuple joinTuples(const Scheme &leftScheme, const Scheme &rightScheme, const Scheme &resultScheme, const Tuple &left, const Tuple &right);
+    std::string crossNames(std::string left, std::string right);
+    const Scheme crossSchemes(const Scheme &left, const Scheme &right);
+    const Tuple crossTuples(const Tuple &left, const Tuple &right);
 
 public:
     Relation(const std::string &_name, const Scheme &_scheme)
@@ -24,15 +30,7 @@ public:
     std::string toString() const;
 
     const Relation join(const Relation &right);
-    std::string joinNames(std::string left, std::string right);
-    const Scheme joinSchemes(const Scheme &left, const Scheme &right);
-    const Tuple joinTuples(const Scheme &leftScheme, const Scheme &rightScheme, const Scheme &resultScheme, const Tuple &left, const Tuple &right);
-
     const Relation cross(const Relation &right);
-    std::string crossNames(std::string left, std::string right);
-    const Scheme crossSchemes(const Scheme &left, const Scheme &right);
-    const Tuple crossTuples(const Tuple &left, const Tuple &right);
-
     const Relation selectForConstant(unsigned int u_index, const std::string &value) const;
     const Relation selectForEqualVariables(unsigned int u_index1, unsigned int u_index2) const;
     const Relation project(const std::vector<unsigned int> &u_indices) const;
