7eafc06d1f82cc07853f4611a287f877c4e4593e
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-28 16:08:04 -0700

    Reorganized file and updated function scopes

--- Relation.h
+++ Relation.h
@@ -9,38 +9,48 @@
 class Relation
 {
 private:
+    // Private variables
     std::string name;
     Scheme scheme;
     std::set<Tuple> tuples;
-    bool contains(std::vector<std::string> strs, std::string str);
+
+    // join() functions
     std::string joinNames(std::string left, std::string right);
     const Scheme joinSchemes(const Scheme &left, const Scheme &right);
     const Tuple joinTuples(const Scheme &leftScheme, const Scheme &rightScheme, const Tuple &left, const Tuple &right);
+
+    // cross() functions
     std::string crossNames(std::string left, std::string right);
     const Scheme crossSchemes(const Scheme &left, const Scheme &right);
     const Tuple crossTuples(const Tuple &left, const Tuple &right);
 
-public:
-    Relation(const std::string &_name, const Scheme &_scheme)
-        : name(_name), scheme(_scheme){};
-
+    // Misc
+    bool contains(std::vector<std::string> strs, std::string str);
     static bool joinable(const Scheme &leftScheme, const Scheme &rightScheme,
                          const Tuple &leftTuple, const Tuple &rightTuple);
 
-    void addTuple(const Tuple &tuple);
-    std::string toString() const;
+public:
+    // Constructors
+    Relation(const std::string &_name, const Scheme &_scheme)
+        : name(_name), scheme(_scheme){};
 
+    // Manipulators
+    void addTuple(const Tuple &tuple);
+
+    // Accessors
+    Scheme getScheme() const { return scheme; }
+    std::string getName() const { return name; }
+    std::set<Tuple> getTuples() { return tuples; }
+    std::string toString() const;
+    int size() const { return static_cast<int>(tuples.size()); }
+
+    // Mathematical Relation operations
     const Relation join(const Relation &right);
     const Relation cross(const Relation &right);
     const Relation selectForConstant(unsigned int u_index, const std::string &value) const;
     const Relation selectForEqualVariables(unsigned int u_index1, unsigned int u_index2) const;
     const Relation project(const std::vector<unsigned int> &u_indices) const;
     const Relation rename(std::vector<std::string> &newNames) const;
-
-    Scheme getScheme() const { return scheme; }
-    std::string getName() const { return name; }
-    int size() const { return static_cast<int>(tuples.size()); }
-    std::set<Tuple> getTuples() { return tuples; }
 };
 
 #endif
