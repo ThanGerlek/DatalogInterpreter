36a3b5e19c5c9be66d854b15924c8875f57f6f38
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-28 18:28:16 -0700

    Changed getTableName() to use head.getId(), not head.toString()

--- Rule.h
+++ Rule.h
@@ -12,7 +12,7 @@ public:
     ~Rule(){};
 
     Predicate getHead() const { return head; }
-    std::string getTableName() const { return head.toString(); }
+    std::string getTableName() const { return head.getId(); }
     const std::vector<Predicate> &getPredicates() const { return body; }
     void addPredicate(Predicate p) { body.push_back(p); }
 
