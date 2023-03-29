6603158c5d08aacd7f4b1cf60d10d3ec36155399
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-28 23:19:03 -0700

    Added getAttributeIndex() and hasAttribute()

--- Scheme.h
+++ Scheme.h
@@ -5,6 +5,8 @@
 #include <string>
 #include <sstream>
 
+// TODO Standardize variable names and signedness
+
 class Scheme : public std::vector<std::string>
 {
 public:
@@ -24,6 +26,31 @@ public:
         }
         out << ")";
         return out.str();
+    }
+
+    unsigned int getAttributeIndex(std::string attribute) const
+    {
+        for (int i = 0; i < this->size(); i++)
+        {
+            if (this->at(i) == attribute)
+            {
+                return i;
+            }
+        }
+        std::cout << "[ERROR] getAttributeIndex() couldn't find given attribute." << std::endl;
+        throw;
+    }
+
+    bool hasAttribute(std::string &attribute) const
+    {
+        for (std::string currentAttribute : *this)
+        {
+            if (currentAttribute == attribute)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
