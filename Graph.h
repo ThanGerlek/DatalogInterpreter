40d78aaa12afd4437a9e39539068c50ef928c7dd
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2023-03-30 23:09:18 -0700

    Added Graph class

---
+++ Graph.h
@@ -0,0 +1,38 @@
+#ifndef CS236_LAB5_T2K_DATALOG_GRAPH_H
+#define CS236_LAB5_T2K_DATALOG_GRAPH_H
+
+#include <map>
+#include "Node.h"
+
+class Graph
+{
+private:
+    std::map<int, Node> nodes;
+    int numNodes;
+
+public:
+    Graph(int _numNodes) : numNodes(_numNodes)
+    {
+        for (int i = 0; i < numNodes; i++)
+        {
+            nodes.at(i) = Node();
+        }
+    };
+
+    void addEdge(int fromNodeId, int toNodeId)
+    {
+        nodes.at(fromNodeId).addEdge(toNodeId);
+    }
+
+    std::string toString() const
+    {
+        std::stringstream out;
+        for (int i = 0; i < numNodes; i++)
+        {
+            out << "R" << i << ":" << nodes.at(i).toString() << std::endl;
+        }
+        return out.str();
+    }
+};
+
+#endif
